c156827849d9bfdc497938690bf77f09e39c3d3d
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-05-06 21:28:22 -0700

    handlebars setup

---
+++ dist/scripts/all.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var menuApp = new Vue({
+    el: '.nav',
+    data: {
+        menuOpen: false
+    }
+});
+
+var homeApp = new Vue({
+    el: '.home-grid',
+    data: {
+        sliderTime: 6000,
+        interval: ''
+    },
+    methods: {
+        nextSlide: function nextSlide() {
+            var active = $('.slide.active');
+            active.removeClass('active');
+            if (active.is($('.slide').last())) {
+                $('.slide').first().addClass('active');
+            } else {
+                active.next('.slide').addClass('active');
+            }
+            clearInterval(this.interval);
+            this.timerSet();
+        },
+        prevSlide: function prevSlide() {
+            var active = $('.slide.active');
+            active.removeClass('active');
+            if (active.is($('.slide').first())) {
+                $('.slide').last().addClass('active');
+            } else {
+                active.prev('.slide').addClass('active');
+            }
+            clearInterval(this.interval);
+            this.timerSet();
+        },
+        timerSet: function timerSet() {
+            this.interval = setInterval(function () {
+                homeApp.nextSlide();
+            }.bind(this), this.sliderTime);
+        }
+    },
+    mounted: function mounted() {
+        this.timerSet();
+    }
+});
+
+function loadAnimations() {
+    var controller = new ScrollMagic.Controller({});
+
+    $('.animate').each(function () {
+        new ScrollMagic.Scene({
+            triggerElement: this
+        }).setClassToggle(this, 'reveal')
+        // .addIndicators()
+        .offset(-80).reverse(false).addTo(controller);
+    });
+}
+
+$(document).ready(function () {
+    $('.home-grid .slide').first().addClass("active");
+    $('.nav .menu .menu-button').click(function () {
+        $('body').toggleClass('menu-open');
+    });
+
+    loadAnimations();
+    // $('[data-parallax="scroll"]').each(function(){
+    //     $(this).parallax({
+    //         imageSrc: $(this).attr('data-image-src'),
+    //         speed: 0.8
+    //     });
+    // });
+});
+
+// document.addEventListener("turbolinks:click", function() {
+//     $('.page-trans').addClass("animate");
+// });
+//
+// document.addEventListener("turbolinks:load", function() {
+// $('.page-trans').addClass("animate");
+// });
