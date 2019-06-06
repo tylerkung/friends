58b85b36a1bd88e1f7eb487ce55eb728dcd407da
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2019-06-05 20:51:20 -0700

    Media page ready

---
+++ dist/data/media.js
@@ -0,0 +1,92 @@
+//'big' items can be on any column except the last
+//'tall' items can be on any column
+//'wide' items can only be on columns 1, 4
+//all special 'attr' items should be wary of each other
+
+export default{
+    "data": [
+        {
+            "imageURL": '/img/childish-1.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/childish-2.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/mike-4.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/childish-4.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/duke-1.jpg',
+            "attr":{
+                "big": 'true'
+            }
+        },
+        {
+            "imageURL": '/img/childish-3.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/frank-ocean-1.jpg',
+            "attr": {
+                "big": 'true'
+            }
+        },
+        {
+            "imageURL": '/img/saweetie-2.jpg',
+            "attr": {
+                "tall": 'true'
+            }
+        },
+        {
+            "imageURL": '/img/duke-3.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/frank-ocean-2.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/kaytranada.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/duke-2.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/kaytranada-2.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/kaytranada-3.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/wuzhen-1.jpg',
+            "attr": {
+            }
+        },
+        {
+            "imageURL": '/img/mike-3.jpg',
+            "attr": {
+            }
+        }
+    ]
+}
